c474e5fd20c955a23e0dc1e0b81cbee704bde1de
alvesxdani <dani.alvesm22@gmail.com>
2024-01-28 19:16:16 -0300

    :lipstick: adicionando a variável space grotesk na página

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { grotesk } from "./fonts/fonts";
 
 export const metadata: Metadata = {
   title: "Loki - A série",
@@ -16,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={`${grotesk.variable}`}>{children}</body>
     </html>
   );
 }
