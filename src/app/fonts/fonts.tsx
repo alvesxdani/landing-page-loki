e30350e9de6e6893f7f78eeeb40851426c5bc238
alvesxdani <dani.alvesm22@gmail.com>
2024-01-28 19:15:25 -0300

    :lipstick: adicionando space grotesk font

---
+++ src/app/fonts/fonts.tsx
@@ -0,0 +1,9 @@
+import { Space_Grotesk } from 'next/font/google'
+
+const grotesk = Space_Grotesk({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--space-grotesk',
+})
+
+export { grotesk }
