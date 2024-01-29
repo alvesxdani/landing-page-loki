c18a4774405adbf4efe4c7a0b90542a4ad2a926e
alvesxdani <dani.alvesm22@gmail.com>
2024-01-28 22:23:09 -0300

    :sparkles: feat: add seção de recomendados

--- src/app/components/Sections/Recommendations.tsx
+++ src/app/components/Sections/Recommendations.tsx
@@ -1,8 +1,51 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Recommendations = () => {
   return (
-    <div>Recommendations</div>
+    <section
+      id="recommendations"
+      className="flex flex-col justify-center items-center w-full min-h-[100vh] bg-[#111] p-6 gap-10"
+    >
+      <h2 className="text-4xl font-bold font-space">Só no Disney+</h2>
+      <div className="flex flex-col md:flex-row gap-4 flex-wrap justify-center">
+        <Image
+          src="/Images/poster1.png"
+          alt="The Mandalorian"
+          width={300}
+          height={200}
+          className="w-[200px]"
+        />
+        <Image
+          src="/Images/poster2.png"
+          alt="The Mandalorian"
+          width={300}
+          height={287}
+          className="w-[200px]"
+        />
+        <Image
+          src="/Images/poster3.png"
+          alt="The Mandalorian"
+          width={300}
+          height={287}
+          className="w-[200px]"
+        />
+        <Image
+          src="/Images/poster4.png"
+          alt="The Mandalorian"
+          width={300}
+          height={287}
+          className="w-[200px]"
+        />
+        <Image
+          src="/Images/poster5.png"
+          alt="The Mandalorian"
+          width={300}
+          height={287}
+          className="w-[200px]"
+        />
+      </div>
+    </section>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Intro from "./components/Sections/Intro";
+import Recommendations from "./components/Sections/Recommendations";
 
 
 export default function Home() {
   return (
     <main>
       <Intro />
+      <Recommendations />
     </main>
   )
 }
