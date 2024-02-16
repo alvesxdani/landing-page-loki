d1aab92588270a6783bcc9ae479501f749e8ecaf
alvesxdani <dani.alvesm22@gmail.com>
2024-02-16 16:47:19 -0300

    :wrench: add image config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
+  images: {
+    deviceSizes: [768, 1024, 1280],
+    // Enable to use the Next.js Image Optimization API route - /_next/image/{hash}/{width}/{height}/*
+    onlineImageOptimization: true,
+    // The path where image-config.json will be created, relative to process.cwd().
+    path: './public',
+    // https://github.com/vercel/next.js/pull/29539
+    disableStaticImages: false,
+  },
 };
 
 export default nextConfig;
